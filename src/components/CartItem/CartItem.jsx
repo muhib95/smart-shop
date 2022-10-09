448dbbf0f0372cb46a15b2a7922623009bebea94
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-09 23:31:51 +0600

    final riv

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -19,7 +19,7 @@ const CartItem = ({product,removeItem}) => {
 						</div>
 					</div>
 					<div className="flex text-sm divide-x">
-						<button onClick={()=>removeItem()} type="button" className="flex items-center px-2 py-1 pl-0 space-x-1">
+						<button onClick={()=>removeItem(product)} type="button" className="flex items-center px-2 py-1 pl-0 space-x-1">
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 h-4 fill-current">
 								<path d="M96,472a23.82,23.82,0,0,0,23.579,24H392.421A23.82,23.82,0,0,0,416,472V152H96Zm32-288H384V464H128Z"></path>
 								<rect width="32" height="200" x="168" y="216"></rect>
